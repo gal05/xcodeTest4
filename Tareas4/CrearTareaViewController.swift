b8d81b455db3f3adc98bbffa96ad3ed012100665
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-09-19 16:10:30 -0500

    hasta la pagina 10

---
+++ Tareas4/CrearTareaViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CrearTareaViewController.swift
+//  Tareas4
+//
+//  Created by Guerra Jose on 19/09/18.
+//  Copyright © 2018 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class CrearTareaViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var txtNombreTarea: UITextField!
+    
+    
+    @IBOutlet weak var swImportante: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    @IBAction func agregar(_ sender: Any) {
+        let tarea=Tarea()
+        tarea.nombre=txtNombreTarea.text!
+        tarea.importante=swImportante.isOn
+    }
+
+    
+
+}
